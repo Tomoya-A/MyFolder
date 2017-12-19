fb38073ca8ae0c3e340b60978883beebdbae249e
Tomoya-A <34120639+Tomoya-A@users.noreply.github.com>
2017-12-19 16:47:26 +0900

    Add files via upload

---
+++ kadai1/kadai1.m
@@ -0,0 +1,33 @@
+% ‰Û‘è‚P@•W–{‰»ŠÔŠu‚Æ‹óŠÔ‰ğ‘œ“x
+
+clear; % •Ï”‚ÌƒI[ƒ‹ƒNƒŠƒA
+
+ORG=imread('kadai1.jpg'); % Œ´‰æ‘œ‚Ì“ü—Í
+imagesc(ORG); axis image; % ‰æ‘œ‚Ì•\¦
+pause; % ˆê’â~
+
+IMG = imresize(ORG,0.5); % ‰æ‘œ‚Ìk¬
+IMG2 = imresize(IMG,2,'box'); % ‰æ‘œ‚ÌŠg‘å
+imagesc(IMG2); axis image; % ‰æ‘œ‚Ì•\¦
+pause; % ˆê’â~
+
+IMG = imresize(IMG,0.5); % ‰æ‘œ‚Ìk¬
+IMG2 = imresize(IMG,4,'box'); % ‰æ‘œ‚ÌŠg‘å
+imagesc(IMG2); axis image; % ‰æ‘œ‚Ì•\¦
+pause; % ˆê’â~
+
+IMG = imresize(IMG,0.5); % ‰æ‘œ‚Ìk¬
+IMG2 = imresize(IMG,8,'box'); % ‰æ‘œ‚ÌŠg‘å
+imagesc(IMG2); axis image; % ‰æ‘œ‚Ì•\¦
+pause; % ˆê’â~
+
+IMG = imresize(IMG,0.5); % ‰æ‘œ‚Ìk¬
+IMG2 = imresize(IMG,16,'box'); % ‰æ‘œ‚ÌŠg‘å
+imagesc(IMG2); axis image; % ‰æ‘œ‚Ì•\¦
+pause; % ˆê’â~
+
+IMG = imresize(IMG,0.5); % ‰æ‘œ‚Ìk¬
+IMG2 = imresize(IMG,32,'box'); % ‰æ‘œ‚ÌŠg‘å
+imagesc(IMG2); axis image; % ‰æ‘œ‚Ì•\¦
+
+return
